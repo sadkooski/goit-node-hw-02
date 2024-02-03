6ceb23f3c1f4372d594d0db5e18d7e407f589878
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2024-02-03 03:28:58 +0100

    Require mongoose

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 const path = require("path");
+const mongoose = require('mongoose');
+const connectDB = require('./server')
+
+require('dotenv').config()
 
 const contactsRouter = require("./routes/api/contacts");
 
@@ -9,14 +13,21 @@ const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
+const connection = mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("Connected to MongoDB"))
+.catch((err) => console.error("Error connecting to MongoDB:", err));;
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-
 app.use("/api/contacts", contactsRouter);
+
+
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
