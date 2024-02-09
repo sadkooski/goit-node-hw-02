fe765d6a798fe2ee7c9b43a4c3c67c0472b5d331
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2024-02-09 14:58:24 +0100

    2

--- server.js
+++ server.js
@@ -1,32 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
-const fs = require('fs').promises
 const cors = require("cors");
 const path = require("path");
 const mongoose = require('mongoose');
-const multer = require('multer')
 require('dotenv').config()
 require('./modules/auth/auth.strategy')
-
-const uploadDir = path.join(process.cwd(), 'public/avatars')
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadDir);
-  },
-  filename: (req, file, cb) => {
-    cb(null, file.originalname);
-  },
-  limits: {
-    fileSize: 1048576,
-  },
-});
-
-const uploadMiddleware = multer({
-  storage,
-});
-
-
 
 const contactsRouter = require("./modules/contacts/contacts.routing");
 const authRouter = require("./modules/auth/auth.routing")
@@ -44,19 +22,6 @@ const connection = mongoose.connect(process.env.DATABASE_URL, {
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-const isAccessible = (folderPath) => {
-  return fs
-    .access(folderPath)
-    .then(() => true)
-    .catch(() => false);
-};
-
-const createFolderIsNotExist = async (folderPath) => {
-  if (!(await isAccessible(folderPath))) {
-    await fs.mkdir(folderPath);
-  }
-};
-
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
@@ -64,9 +29,6 @@ app.use(express.static('public'))
 
 app.use("/auth", authRouter);
 app.use("/api/contacts", contactsRouter);
-app.post('/api/users/avatars', uploadMiddleware.single('photo'), (req, res) => {
-  return res.status(200).json({ message: 'Plik wrzucony pomyślnie'})
-})
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
@@ -76,6 +38,5 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(3000, () => {
-  createFolderIsNotExist(uploadDir);
   console.log("Server running. Use our API on port: 3000")
 })
